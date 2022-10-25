274e8f044f5faf3a1f0909d40367cf210e1f456f
“miriamSow” <miriamsow@hotmail.com>
2022-10-25 22:04:21 +0100

    Tarefa1 commit

---
+++ Tarefa01Vm.js
@@ -0,0 +1,8 @@
+const Tempo= 1.5 ;
+var Dist= 60 ; 
+const veloMedia= Dist/Tempo
+
+
+console.log( "Oi sou a Miriam Vou te ajudar a calcular sobre MRU kk" );
+console.log("Só um minuto que estou pensando hehe...");
+console.log("Nossa você andou por : " + veloMedia + "km/h ");
